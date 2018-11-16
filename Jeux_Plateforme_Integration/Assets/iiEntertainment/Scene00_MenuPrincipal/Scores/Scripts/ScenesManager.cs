aaab16de34aae1bbd3d512982d1dc98e6bdec715
SilverDDX <yannis.guillemote@gmail.com>
2018-11-16 19:26:50 +0100

    First Commit

---
+++ Jeux_Plateforme_Integration/Assets/iiEntertainment/Scene00_MenuPrincipal/Scores/Scripts/ScenesManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour {
+	private ScoresManager ScoresManager;
+	private ScoreScene currentScoreScene;
+	// Use this for initialization
+	void Awake(){
+		
+	}
+	void Start () {
+
+		//Destroys the GameObject if it already exists
+		DontDestroyOnLoad (this.gameObject);
+		if (FindObjectsOfType (GetType ()).Length > 1) {
+			Destroy (this.gameObject);
+		}
+		ScoresManager = GetComponent<ScoresManager> ();
+	}
+	
+	public void BackToMenu(){
+		currentScoreScene = GameObject.Find("[Score_scene]").GetComponent<ScoreScene>();
+		ScoresManager.SaveScore(SceneManager.GetActiveScene().buildIndex-1, currentScoreScene.score);
+		if(SceneManager.GetActiveScene().buildIndex !=0)
+		SceneManager.LoadScene(0);
+	}
+
+	void LoadScene(int index){
+		SceneManager.LoadScene(index);
+	}
+
+	void Update(){
+		
+		if (Input.GetKeyDown(KeyCode.Z)) {
+			Debug.Log ("backmenu");
+			BackToMenu ();
+		}
+
+	
+		if (Input.GetKeyDown(KeyCode.E)) {
+			Debug.Log ("sc1");
+			LoadScene (1);
+		}
+
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Debug.Log ("sc2");
+			LoadScene (2);
+		}
+
+
+	}
+}
---
+++ Jeux_Plateforme_Integration/Assets/iiEntertainment/Scene00_MenuPrincipal/Scores/Scripts/UIFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFunctions : MonoBehaviour {
+	public InputField input;
+	public Text currentName;
+	// Use this for initialization
+	void Start () {
+	//Keeps UI over scenes
+		DontDestroyOnLoad (this.gameObject);
+		if (FindObjectsOfType (GetType ()).Length > 1) {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	public void SetUserName(){
+		currentName.text = input.text;
+	}
+}
